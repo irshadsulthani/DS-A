5cce2f7a41cda3ecfe5cb21da498665f9632154e
irshadsulthani <irshadpallar2020@gmail.com>
2024-11-27 11:36:11 +0530

    changes

--- DSA3/tree.js
+++ DSA3/tree.js
@@ -1,3 +1,46 @@
+/**
+ * ! Tree 
+ * * it's a non-linear Data Structure
+ * * conncted by nodes edges
+ * * Hirerarchial Relationships between elements.
+ * ? eg:- family tree, file directory system, organizational chart.
+ * 
+ * ! Features of Tree
+ * 
+ * * Root Node:
+ * ? The topmost Node of a tree
+ * ? Only one root in a Node tree
+ * 
+ * * Parant and Child Nodes:
+ * ? The Node directly connected to a above called it's called parent.
+ * ? Node directly connected below nodes that's are children
+ * 
+ * * Edge:
+ * ? A connection between two nodes in the tree
+ * 
+ * * Leaf Node:
+ * ? A node that has no children
+ * 
+ * * Subtree:
+ * ? Any node along with its descendats forms a subtree
+ * 
+ * * Height of Tree:
+ * ? The length of the longest path from the root node to a leaf.
+ * 
+ * * Depth of a Node:
+ * ? The number of edges from the root node to the node.
+ * 
+ * * Binary Tree:
+ * ? Each node has at most two children.
+ * ? referred to as the left child and right child.
+ * 
+ * ! Application of Tree:-
+ * 
+ * * Binary Search Tree(Used for Searching and sorting data efficiently.)
+ * * File System (Represent hierarchiel file structure.)
+ * * Network Routing (Represent network hierarchies.)
+ * * Tries (Used for efficient storage and retrieval of strings)
+ */
 
-//? it's a non-linear Data Structure
+
 
