55163d144b3caf65b7a28c765b2bd3ff4d02025d
irshadsulthani <irshadpallar2020@gmail.com>
2024-12-06 14:32:16 +0530

    some new things here

--- DSA3/graph.js
+++ DSA3/graph.js
@@ -1,1 +1,34 @@
-lkadsyigflhasflg
+class Graph {
+    constructor() {
+        this.list = {}
+    }
+    addVertex(vertex) {
+        if (!this.list[vertex]) {
+            this.list[vertex] = []
+        }
+    }
+    addEdge(v1, v2) {
+        if (this.list[v1]) {
+            this.list[v1].push(v2)
+        }
+        if (this.list[v2]) {
+            this.list[v2].push(v1)
+        }
+    }
+    dfs(start) {
+        const result = []
+        const visit = new Set()
+
+        function explore(vertex) {
+            if (!visit.has(vertex)) {
+                visit.add(vertex)
+                result.push(vertex)
+                for(let near of this.list[vertex]){
+                    explore(near)
+                }
+            }
+        }
+        explore.call(this, start)
+        return result;
+    }
+}
